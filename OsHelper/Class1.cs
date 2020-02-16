9bdd4c9670257f0e2237d57e466b3e7ad3822013
Michael Melikov <coutaq@gmail.com>
2020-02-16 15:58:24 +0300

    i guess im done

--- OsHelper/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace OsHelper
-{
-    public class OsHelper
-    {
-
-    }
-}
---
+++ OsHelper/OsHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OsHelper
+{
+    public class OsHelper
+    {
+        /// <summary>
+        /// Formats the filepath string according to your os.
+        /// </summary>
+        /// <returns>
+        /// A formatted string.
+        /// </returns>
+        /// <param name="filepath">The string that will be formatted.</param>
+        /// <param name="relativePath">Is filepath relative to the executive or absolute.</param>
+        public static String CompatiblePath(String filepath, Boolean relativePath)
+        {
+            String newFilePath = filepath;
+            Boolean Unix = System.Environment.OSVersion.ToString().Contains("Unix");
+            if (Unix)
+                newFilePath = newFilePath.Replace('\\','/');
+            else
+                newFilePath = newFilePath.Replace('/', '\\');
+            //TODO: come up with a better name for "relative path"
+            if (relativePath)
+                if (Unix)
+                    newFilePath = "/" + newFilePath;
+                else
+                    newFilePath = ".\\" + newFilePath;
+            return newFilePath;
+        }
+    }
+}
